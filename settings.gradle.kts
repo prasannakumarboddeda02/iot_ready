555b0fbd196bd7cef8045d67d5d8bfc9699a3ca3
prasannakumarboddeda <prasannakumarboddeda@gmail.com>
2023-10-01 10:56:58 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Iot_Ready"
+include(":app")
